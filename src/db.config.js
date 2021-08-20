6f35c3ce9fd100dfeab9f953ede36372470ea3d6
nmp3458 <nmp3458@gmail.com>
2021-08-20 23:30:34 +0530

    Connect to database

--- src/db.config.js
+++ src/db.config.js
@@ -1,8 +1,15 @@
 var { Client } = require("pg");
+// .env file can be use
+require("dotenv").config();
 
 const createDatabaseConnection = () => {
   const connectionString = process.env.DATABASE_URL;
-  const client = new Client({ connectionString });
+  const client = new Client({
+    connectionString,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
   client.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
